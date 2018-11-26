abd0f8be408e9065ec82b318645bdb4c8fbf6225
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-11-26 06:56:59 +0100

    Better orbits

--- code/gameinitialize.pas
+++ code/gameinitialize.pas
@@ -106,9 +106,9 @@ begin
   );
 
   { Initialize animations. }
-  LightOrbit := TOrbitAnimation.Create(Application, 4);
-  MoonOrbit := TOrbitAnimation.Create(Application, 2.5);
-  SatelliteOrbit := TOrbitAnimation.Create(Application, 1.5);
+  LightOrbit := TOrbitAnimation.Create(Application, 5);
+  MoonOrbit := TOrbitAnimation.Create(Application, 3);
+  SatelliteOrbit := TOrbitAnimation.Create(Application, 3);
   EarthRotation := TRotationAnimation.Create(Application, 0.2);
   MoonRotation := TRotationAnimation.Create(Application, 0.8);
   SatelliteRotation := TRotationAnimation.Create(Application, 1);
